27729e517be77016a8997e2745d5d6176e9681a4
vinci72 <43184904+vinci72@users.noreply.github.com>
2022-01-06 21:18:33 +0100

    Rename

---
+++ scripts/customize_install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#Do System Update
+apt update -y
+apt upgrade -y 
+
+#Package Installation
+apt install mc open-vm-tools net-tools iptraf-ng cifs-utils genisoimage squashfs-tools xorriso ufw curl wget -y
+
+#SSH Public Key
+mkdir /home/jzawadzki/.ssh
+cd /home/jzawadzki/.ssh
+wget https://raw.githubusercontent.com/vinci72/ssh/main/authorized_keys
+cd ..
+chown jzawadzki:jzawadzki /home/jzawadzki/.ssh /home/jzawadzki/.ssh/authorized_keys
+
+#VEEAM AGENT FREE Instalation
+cd /root
+wget https://github.com/vinci72/ssh/raw/main/veeam-release-deb_1.0.8_amd64.deb
+
+dpkg -i ./veeam-release* && apt-get update
+apt install veeam -y
+
+#Firewall with SSH allow
+ufw allow ssh
+ufw default deny incoming
+ufw default allow outgoing
+ufw enable
+
+#Disable Password SSH Logins
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin prohibit-password/g' /etc/ssh/sshd_config
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+service sshd restart
+
+#Get step2.sh script
+
