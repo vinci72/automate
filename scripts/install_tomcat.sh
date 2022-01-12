9cc464277866840a3fa4b7fe1152d3d8af600ba3
vinci72 <43184904+vinci72@users.noreply.github.com>
2022-01-12 19:14:12 +0100

    powrot do wersji bez zmiennych

--- scripts/install_tomcat.sh
+++ scripts/install_tomcat.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# START VARIABLES
-#JAVA= max system memory for JAVA in MB
-#
-ADMIN="tajniak"
-PASS="tajnehaslo"
-MAXMEM=4196
-# END VARIABLES
-
 #Do System Update
 apt update -y
 apt update -y 
@@ -18,9 +10,9 @@ cd /srv
 ln -s /var/lib/tomcat9/ tomcat
 
 echo "# Ustawienie pamieci JAVA" >> /etc/default/tomcat9
-echo "JAVA_OPTS=\"\${JAVA_OPTS} -Xms1024m -Xmx$MAXMEMm -XX:MaxPermSize=512m\"" >> /etc/default/tomcat9
+echo "JAVA_OPTS=\"\${JAVA_OPTS} -Xms1024m -Xmx4196m -XX:MaxPermSize=512m\"" >> /etc/default/tomcat9
 
-sed -i '/<*tomcat-users>/i <user username=\"$ADMIN\" password=\"$PASS\" roles="admin,manager,manager-gui,manager-script,admin-gui"/>' /srv/tomcat/conf/tomcat-users.xml
+sed -i '/<*tomcat-users>/i <user username="tajniak" password="tajnehaslo" roles="admin,manager,manager-gui,manager-script,admin-gui"/>' /srv/tomcat/conf/tomcat-users.xml
 
 echo "updating UFW ... "
 ufw allow 8080/tcp
